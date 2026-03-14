231ac5388df910c86ec4853f8bb7f7fad9dbc831
prashanth <prashanth2957@gmail.com>
2026-03-14 14:57:25 +0530

    Enhance image upload and prediction display

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from ultralytics import YOLO
 import streamlit as st
+from ultralytics import YOLO
 from PIL import Image
+import numpy as np
 
-st.title("Fake Brand Detection")
+st.title("Fake Brand Detection using Deep Learning")
 
 model = YOLO("best.pt")
 
-uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload Image", type=["jpg","jpeg","png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image")
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    results = model.predict(image)
+    img = np.array(image)
+
+    results = model.predict(img)
 
     probs = results[0].probs
     names = results[0].names
 
-    top1 = probs.top1
-    confidence = probs.top1conf
+    predicted_class = names[probs.top1]
+    confidence = float(probs.top1conf)
 
-    st.success(f"Prediction: {names[top1]}")
+    st.success(f"Prediction: {predicted_class}")
     st.write(f"Confidence: {confidence:.2f}")
