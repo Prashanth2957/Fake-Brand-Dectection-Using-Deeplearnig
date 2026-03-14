2826babe422cd2d3ac3d0fe287a40891589310b7
prashanth <prashanth2957@gmail.com>
2026-03-14 14:32:58 +0530

    Refactor image upload and prediction display logic

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-import streamlit as st
 from ultralytics import YOLO
+import streamlit as st
 from PIL import Image
 
-# Load trained model
+st.title("Fake Brand Detection")
+
 model = YOLO("best.pt")
 
-st.title("Counterfeit Brand Detection")
-
-uploaded_file = st.file_uploader("Upload Shoe Image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
-
     image = Image.open(uploaded_file)
-
     st.image(image, caption="Uploaded Image")
 
     results = model.predict(image)
 
-    probs = results[0].probs.data.tolist()
+    probs = results[0].probs
     names = results[0].names
 
-    predicted_class = names[probs.index(max(probs))]
-    confidence = max(probs)
+    top1 = probs.top1
+    confidence = probs.top1conf
 
-    st.success(f"Prediction: {predicted_class}")
+    st.success(f"Prediction: {names[top1]}")
     st.write(f"Confidence: {confidence:.2f}")
